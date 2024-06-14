f0e0481e65e47f7eb9b1b91feddfb878712bd7e9
WooJJam <jaemin5548@naver.com>
2024-06-14 15:47:17 +0900

    [section 1] 소개 및 개요 - Spring Security 의존성 추가

--- src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
+++ src/main/java/com/eazybytes/springsecuritybasic/controller/WelcomeController.java
@@ -3,11 +3,16 @@ package com.eazybytes.springsecuritybasic.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Spring Security 의존성 추가시 내부적으로 모든 요청을 보호함
+ * 초기 id: user
+ * password: 콘솔 확인
+ */
 @RestController
 public class WelcomeController {
 
     @GetMapping("/welcome")
     public String sayWelcome() {
-        return "Welcome to Spring Application with out Security";
+        return "Welcome to Spring Application with Security";
     }
 }
