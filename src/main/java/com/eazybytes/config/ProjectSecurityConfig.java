f7ec9077940fe77f4e89299ed36222a78285e4f2
WooJJam <jaemin5548@naver.com>
2024-06-22 22:43:14 +0900

    [section 3] 유저 정의와 관리 - InMemoryUserDetailsManage를 사용한 유저 설정 방식 2

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,19 +33,42 @@ public class ProjectSecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        String password = encoder.encode("password");
+         /* Approach 1 where we use withDefaultPasswordEncoder() method
+        while creating the user details /*
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        String password = encoder.encode("password");
+//
+//        UserDetails admin = User.withUsername("admin")
+//                .password(password)
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User.withUsername("user")
+//                .password(password)
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+     /* Approach 2 where we use NoOpPasswordEncoder Bean
+		while creating the user details*/
 
         UserDetails admin = User.withUsername("admin")
-                .password(password)
+                .password("12345")
                 .authorities("admin")
                 .build();
 
         UserDetails user = User.withUsername("user")
-                .password(password)
+                .password("12345")
                 .authorities("read")
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/eazybytes/controller/LoansController.java
+++ src/main/java/com/eazybytes/controller/LoansController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoansController {
 
-    @GetMapping("/myLons")
+    @GetMapping("/myLoans")
     public String getLoansDetail() {
         return "Here are the card details from the DB";
     }
