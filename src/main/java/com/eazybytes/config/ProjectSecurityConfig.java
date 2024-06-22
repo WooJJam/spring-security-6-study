2aad609bcf6e34b77f4eb2222f1f3897a8ba0e95
WooJJam <jaemin5548@naver.com>
2024-06-22 19:18:38 +0900

    [section 3] 유저 정의와 관리 - InMemoryUserDetailsManage를 사용한 유저 설정 방식 1

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -23,5 +28,23 @@ public class ProjectSecurityConfig {
                 .httpBasic(Customizer.withDefaults());
         return http.build();
 
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        String password = encoder.encode("password");
+
+        UserDetails admin = User.withUsername("admin")
+                .password(password)
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password(password)
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
