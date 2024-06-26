66656171b0a0df7558198a307d9b87aae359e4e0
WooJJam <jaemin5548@naver.com>
2024-06-26 23:07:01 +0900

    [section 3] 유저 정의와 관리 - Custom UserDetailService 구현

---
+++ src/main/java/com/eazybytes/model/Customer.java
@@ -0,0 +1,49 @@
+package com.eazybytes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String email;
+    private String pwd;
+    private String role;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
